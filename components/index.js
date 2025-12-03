4e1cc85bc784f90dc22d956f182bd3cb9d75c70e
Eman Salama <eg1712@gmail.com>
2025-12-03 06:01:02 +0200

    Details Screen

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import MeditationTopDisplay from "./MeditationTopDisplay";
+import { default as Tabs } from "./Tabs";
+import { default as About } from "./About";
+import { default as Footer } from "./Footer";
+
+export {
+
+  MeditationTopDisplay,
+  Tabs,
+  About,
+  Footer,
+
+};
